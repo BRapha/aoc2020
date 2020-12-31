16e99f3de3e23e6818ffaa2d11c831813cafd604
Raphael Blistein <raphael.blistein@gmail.com>
2020-12-31 15:35:24 +0100

    Blistein O(n) algo for finding missing number

--- plane_seat.py
+++ plane_seat.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+import math
+
 
 def parse(row):
     bit_string = "".join(map(lambda c: "1" if c in ("B", "R") else "0", row.strip()))
@@ -15,19 +17,47 @@ def read_input(file_name):
     return array
 
 
-def find_my_seat(seat_nums, max_num):
-    my_seat = max_num
+# O(n) algorithm based on the observation that to find a missing number
+# in an otherwise complete sequence of numbers in random order
+# all numbers must be XOR-ed
+# and the sequence needs to start at an even number
+# and have a length that's a multiple of 4
+def find_missing_seat(seat_nums):
+    min_num = math.inf
+    max_num = 0
+    missing_num = 0
     for num in seat_nums:
-        my_seat = my_seat ^ num
+        missing_num ^= num
+        if num < min_num:
+            min_num = num
+        if num > max_num:
+            max_num = num
 
-    return my_seat
+    # min num must be even
+    if min_num % 2 == 1:
+        min_num = min_num - 1
+        missing_num ^= min_num
+
+    # max num must be odd
+    if max_num % 2 == 0:
+        max_num = max_num + 1
+        missing_num ^= max_num
+
+    count = max_num - min_num + 1  # i.e. including boarders
+
+    # count must a multiple of 4
+    if count % 4 != 0:  # must be 2 then, because count is even
+        missing_num ^= (max_num + 1)
+        missing_num ^= (max_num + 2)
+
+    return missing_num
 
 
 def main():
     seat_nums = read_input("plane_seats.txt")
     highest = max(seat_nums)
     print(f"Highest: {highest}")
-    print(f"My seat: {find_my_seat(seat_nums, highest)}")
+    print(f"My seat: {find_missing_seat(seat_nums)}")
 
 
 if __name__ == "__main__":
